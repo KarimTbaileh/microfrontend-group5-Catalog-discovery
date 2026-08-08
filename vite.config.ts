2e7a9d8a89507d9180af7e0c92b2fab0072ab7e9
KarimTbaileh <karim.ojmw@gmail.com>
2026-08-08 21:26:25 +0300

    Fix process is not defined in MFE bundle

--- vite.config.ts
+++ vite.config.ts
@@ -4,37 +4,41 @@ import tailwindcss from '@tailwindcss/vite';
 import cssInjectedByJs from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig(({ mode }) => {
-  const isMfe = mode === 'mfe';
+    const isMfe = mode === 'mfe';
 
-  return {
-    base: './',
-    plugins: [
-      react(),
-      tailwindcss(),
-      ...(isMfe ? [cssInjectedByJs()] : []),
-    ],
-    build: isMfe
-        ? {
-          outDir: 'dist-mfe',
-          emptyOutDir: true,
-          target: 'es2022',
-          lib: {
-            entry: 'src/mfe.tsx',
-            formats: ['es'],
-            fileName: () => 'luxe-catalog.js',
-          },
-          rollupOptions: {
-            output: {
-              inlineDynamicImports: true,
-            },
-          },
-          assetsInlineLimit: 1024 * 512,
-          cssCodeSplit: false,
-        }
-        : {
-          outDir: 'dist',
-          emptyOutDir: true,
-          target: 'es2022',
+    return {
+        base: './',
+        define: {
+            'process.env.NODE_ENV': JSON.stringify('production'),
+            'process.env': JSON.stringify({ NODE_ENV: 'production' }),
         },
-  };
+        plugins: [
+            react(),
+            tailwindcss(),
+            ...(isMfe ? [cssInjectedByJs()] : []),
+        ],
+        build: isMfe
+            ? {
+                outDir: 'dist-mfe',
+                emptyOutDir: true,
+                target: 'es2022',
+                lib: {
+                    entry: 'src/mfe.tsx',
+                    formats: ['es'],
+                    fileName: () => 'luxe-catalog.js',
+                },
+                rollupOptions: {
+                    output: {
+                        inlineDynamicImports: true,
+                    },
+                },
+                assetsInlineLimit: 1024 * 512,
+                cssCodeSplit: false,
+            }
+            : {
+                outDir: 'dist',
+                emptyOutDir: true,
+                target: 'es2022',
+            },
+    };
 });
